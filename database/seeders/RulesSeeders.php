ed80653ac99eea78a7870b2b16ceb1ed6bca147e
Joannekess <joannekessie@gmail.com>
2024-04-30 20:25:09 +0700

    Database

--- database/seeders/RulesSeeders.php
+++ database/seeders/RulesSeeders.php
@@ -22,83 +22,154 @@ class RulesSeeders extends Seeder
         DB::table('rules')->insert([
             'penyakit_id' => 1,
             'gejala_id' => 2,
-            'CFValue' => 0.2,
+            'CFValue' => 0.8,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 1,
             'gejala_id' => 3,
-            'CFValue' => 0.4,
+            'CFValue' => 0.2,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 1,
             'gejala_id' => 4,
-            'CFValue' => 0.6,
+            'CFValue' => 0.8,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 1,
             'gejala_id' => 5,
+            'CFValue' => 0.6,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 1,
+            'gejala_id' => 6,
+            'CFValue' => 0.6,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 1,
+            'gejala_id' => 7,
+            'CFValue' => 0.6,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 1,
+            'gejala_id' => 8,
             'CFValue' => 0.2,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 1,
+            'gejala_id' => 9,
+            'CFValue' => 1,
         ]);
 
         // P02
         DB::table('rules')->insert([
             'penyakit_id' => 2,
             'gejala_id' => 1,
+            'CFValue' => 0.8,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 2,
+            'gejala_id' => 10,
             'CFValue' => 0.6,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 2,
-            'gejala_id' => 2,
-            'CFValue' => 0.2,
-        ]);
-        DB::table('rules')->insert([
-            'penyakit_id' => 2,
-            'gejala_id' => 3,
-            'CFValue' => 0.2,
-        ]);
-        DB::table('rules')->insert([
-            'penyakit_id' => 2,
-            'gejala_id' => 4,
+            'gejala_id' => 11,
             'CFValue' => 1,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 2,
-            'gejala_id' => 5,
-            'CFValue' => 0.4,
+            'gejala_id' => 12,
+            'CFValue' => 0.8,
         ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 2,
+            'gejala_id' => 13,
+            'CFValue' => 1,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 2,
+            'gejala_id' => 14,
+            'CFValue' => 0.8,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 2,
+            'gejala_id' => 15,
+            'CFValue' => 1,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 2,
+            'gejala_id' => 16,
+            'CFValue' => 1,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 2,
+            'gejala_id' => 17,
+            'CFValue' => 1,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 2,
+            'gejala_id' => 18,
+            'CFValue' => 1,
+        ]);
+
 
         // P03
         DB::table('rules')->insert([
             'penyakit_id' => 3,
-            'gejala_id' => 1,
+            'gejala_id' => 19,
+            'CFValue' => 1,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 3,
+            'gejala_id' => 20,
             'CFValue' => 0.4,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 3,
-            'gejala_id' => 2,
-            'CFValue' => 0.2,
+            'gejala_id' => 21,
+            'CFValue' => 0.4,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 3,
-            'gejala_id' => 3,
+            'gejala_id' => 22,
             'CFValue' => 0.6,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 3,
-            'gejala_id' => 4,
+            'gejala_id' => 23,
             'CFValue' => 0.8,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 3,
-            'gejala_id' => 5,
-            'CFValue' => 0.2,
+            'gejala_id' => 24,
+            'CFValue' => 1,
         ]);
 
         // P04
         DB::table('rules')->insert([
             'penyakit_id' => 4,
             'gejala_id' => 1,
+            'CFValue' => 0.4,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 4,
+            'gejala_id' => 4,
+            'CFValue' => 0.4,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 4,
+            'gejala_id' => 25,
             'CFValue' => 0.2,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 4,
+            'gejala_id' => 26,
+            'CFValue' => 0.4,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 4,
+            'gejala_id' => 3,
+            'CFValue' => 0.6,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 4,
@@ -107,30 +178,30 @@ class RulesSeeders extends Seeder
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 4,
-            'gejala_id' => 3,
-            'CFValue' => 0.2,
+            'gejala_id' => 27,
+            'CFValue' => 0.4,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 4,
-            'gejala_id' => 4,
+            'gejala_id' => 28,
+            'CFValue' => 0.4,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 4,
+            'gejala_id' => 8,
             'CFValue' => 0.6,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 4,
-            'gejala_id' => 5,
+            'gejala_id' => 29,
             'CFValue' => 0.4,
         ]);
 
         // P05
         DB::table('rules')->insert([
             'penyakit_id' => 5,
-            'gejala_id' => 1,
-            'CFValue' => 0.2,
-        ]);
-        DB::table('rules')->insert([
-            'penyakit_id' => 5,
             'gejala_id' => 2,
-            'CFValue' => 0.2,
+            'CFValue' => 0.4,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 5,
@@ -139,13 +210,23 @@ class RulesSeeders extends Seeder
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 5,
-            'gejala_id' => 4,
-            'CFValue' => 0.6,
+            'gejala_id' => 8,
+            'CFValue' => 0.4,
         ]);
         DB::table('rules')->insert([
             'penyakit_id' => 5,
-            'gejala_id' => 5,
+            'gejala_id' => 30,
             'CFValue' => 0.2,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 5,
+            'gejala_id' => 9,
+            'CFValue' => 0.2,
+        ]);
+        DB::table('rules')->insert([
+            'penyakit_id' => 5,
+            'gejala_id' => 31,
+            'CFValue' => 0.4,
         ]);
 
     }
--- database/seeders/SymptomsSeeders.php
+++ database/seeders/SymptomsSeeders.php
@@ -26,17 +26,148 @@ class SymptomsSeeders extends Seeder
         //G03
         DB::table('symptoms')->insert([
             'kodeGejala' => 'G03',
-            'namaGejala' => 'Merasa pusing',
+            'namaGejala' => 'Diare',
         ]);
         //G04
         DB::table('symptoms')->insert([
             'kodeGejala' => 'G04',
-            'namaGejala' => 'Diare',
+            'namaGejala' => 'Nafsu makan menghilang',
         ]);
         //G05
         DB::table('symptoms')->insert([
             'kodeGejala' => 'G05',
-            'namaGejala' => 'Merasa lemas',
+            'namaGejala' => 'Kuning pada area mata',
         ]);
+        //G06
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G06',
+            'namaGejala' => 'Warna mulut menguning',
+        ]);
+        //G07
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G07',
+            'namaGejala' => 'Konjungtivitis (peradangan pada mata)',
+        ]);
+        //G08
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G08',
+            'namaGejala' => 'Tubuh kurus',
+        ]);
+        //G09
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G09',
+            'namaGejala' => 'Lesu',
+        ]);
+        //G10
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G10',
+            'namaGejala' => 'Gelisah atau terganggu',
+        ]);
+        //G11
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G11',
+            'namaGejala' => 'Agresif',
+        ]);
+        //G12
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G12',
+            'namaGejala' => 'Sensitif dan reaktif terhadap rangsangan',
+        ]);
+        //G13
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G13',
+            'namaGejala' => 'Hipersalivasi (produksi air liur berlebih)',
+        ]);
+        //G14
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G14',
+            'namaGejala' => 'Tidak fokus / tidak konsentrasi',
+        ]);
+        //G15
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G15',
+            'namaGejala' => 'Pupil mata melebar',
+        ]);
+        //G16
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G16',
+            'namaGejala' => 'Cenderung di tempat gelap / fotofobia',
+        ]);
+        //G17
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G17',
+            'namaGejala' => 'Reaksi berlebihan terhadap cahaya dan suara',
+        ]);
+        //G18
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G18',
+            'namaGejala' => 'Takut air / hidrophobia',
+        ]);
+        //G19
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G19',
+            'namaGejala' => 'Kerontokan bulu',
+        ]);
+        //G20
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G20',
+            'namaGejala' => 'Kerusakan bulu (patah, kusam, kering)',
+        ]);
+        //G21
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G21',
+            'namaGejala' => 'Sisa kulit kering seperti ketombe',
+        ]);
+        //G22
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G22',
+            'namaGejala' => 'Beberapa bagian kulit mengeras',
+        ]);
+        //G23
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G23',
+            'namaGejala' => 'Beberapa bagian kulit bersisik',
+        ]);
+        //G24
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G24',
+            'namaGejala' => 'Botak pada beberapa bagian tubuh',
+        ]);
+        //G25
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G25',
+            'namaGejala' => 'Pembesaran perut / kembung',
+        ]);
+        //G26
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G26',
+            'namaGejala' => 'Berat badan menurun',
+        ]);
+        //G27
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G27',
+            'namaGejala' => 'Lidahnya pucat / berwarna putih',
+        ]);
+        //G28
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G28',
+            'namaGejala' => 'Ukuran pupil mata tidak normal / tidak sama besar',
+        ]);
+        //G29
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G29',
+            'namaGejala' => 'Sulit menahan buang air kecil',
+        ]);
+        //G30
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G30',
+            'namaGejala' => 'Perut membuncit',
+        ]);
+        //G31
+        DB::table('symptoms')->insert([
+            'kodeGejala' => 'G31',
+            'namaGejala' => 'Terdapat cacing berwarna kuning krem pada muntahan atau kotorannya ',
+        ]);
+
     }
 }
